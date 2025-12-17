8b59180e98257ac209795723b5994dcdefa1dff8
Алег <maslovoleg.by@gmail.com>
2025-12-17 18:51:29 +0100

    changed config for image paths

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const basePath = isProd ? process.env.NEXT_PUBLIC_BASE_PATH : "";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/layout-project-test",
+  basePath: basePath,
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
+
+  assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : "",
 };
 
 export default nextConfig;
