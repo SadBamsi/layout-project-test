de1b54764d7803d2e97790c51e79f9c5bd3daa43
Алег <maslovoleg.by@gmail.com>
2025-12-17 18:10:00 +0100

    added deploy workflow for github

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/layout-project-test",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
